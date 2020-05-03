2a63d379326ce3304cdbf4a56155d2b696d170c1
julianbasilieri <julian_basilieri@hotmail.com>
2020-05-03 20:37:44 -0300

    Como-hacer-una-huerta

---
+++ huerta.js
@@ -0,0 +1,6 @@
+function instagram() {
+    window.open("https://www.instagram.com/parroquial.sanluisgonzaga/?hl=es-la")
+}
+function facebook() {
+    window.open("https://www.facebook.com/sanluis.gonzaga.3382?fref=search&__tn__=%2Cd%2CP-R&eid=ARAXHjyWiaXe_hZwSupdJqN-6t1jwzYHXBgWodZbkGiYh9raio5hliFzg6WAAhGl7catz0QOaRyV_F-w")    
+}
